5de8dbdfffb72283c27935e391c4248cb0302740
Karthiban1991 <karthickguna1991@gmail.com>
2026-03-25 13:45:08 +0530

    Update k8s_service.py

--- app/services/k8s_service.py
+++ app/services/k8s_service.py
@@ -1,10 +1,21 @@
 from kubernetes import client, config
 
 class K8sService:
+
     def __init__(self):
-        config.load_kube_config()
+        try:
+            config.load_incluster_config()
+        except:
+            config.load_kube_config()
+
         self.v1 = client.CoreV1Api()
 
     def list_pods(self):
         pods = self.v1.list_pod_for_all_namespaces()
-        return [{"name": p.metadata.name} for p in pods.items]
+        return [
+            {
+                "name": p.metadata.name,
+                "namespace": p.metadata.namespace
+            }
+            for p in pods.items
+        ]
