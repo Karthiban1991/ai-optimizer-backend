821c5f1ff740ce1e3f3b4e6e9b2468f0719c28da
Karthiban1991 <karthickguna1991@gmail.com>
2026-03-25 13:40:33 +0530

    Update ai_engine.py

--- app/services/ai_engine.py
+++ app/services/ai_engine.py
@@ -1,1 +1,12 @@
+class AIEngine:
 
+    def optimize(self, pods):
+        actions = []
+
+        for p in pods:
+            actions.append({
+                "pod": p["name"],
+                "action": "scale_down"
+            })
+
+        return actions
